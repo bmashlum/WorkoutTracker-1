123d2e9b1d2d81baf78b647d9a72194c56071f11
Gilad berman <giladberman@gilads-mbp-2.attlocal.net>
2018-10-07 20:31:12 -0400

    we are able to login with email and password

--- FitAid App/app/src/main/java/com/example/brad/fitaid/DatabaseHelper.java
+++ FitAid App/app/src/main/java/com/example/brad/fitaid/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.example.brad.fitaid;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -30,4 +32,116 @@ public class DatabaseHelper  extends SQLiteOpenHelper {
         db.execSQL( String.format( "DROP TABLE IF EXISTS%s", TABLE_NAME ) ); //drop old table if exists
         onCreate( db );
     }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    //creating a method below to insert data
+    public boolean insertData(String email, String fname, String lname, String pass){
+        //we will use this SQLDatabase instance to insert our data
+        SQLiteDatabase db = this.getWritableDatabase();
+        //now we need to create an instance of the class ContentValue
+        ContentValues contentValues = new ContentValues();
+        //we will now take this contentValues instance and insert it into the data columns
+        //the first arg is the column name itself, the second is the data itself.
+        contentValues.put(COL_2, email);
+        contentValues.put(COL_3, fname);
+        contentValues.put(COL_4, lname);
+        contentValues.put(COL_5, pass);
+
+        //we then insert our data using the db instance created above
+        //this takes three arguments. The first is the table name, The second is null and the third
+        //is the contentValues which we have created.
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if (result==-1){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    //creating a method that will show all data that has been entered into the database, using
+//elements from the Cursor class. The Cursor interface allows read-write access to the result
+    public Cursor getAllData(){
+        //creating an instance of the database class firstly to allow us to get all the data
+        SQLiteDatabase db = this.getWritableDatabase();
+        //now we will create an instance of the Cursor class called result and use the
+        //rawQuery method. Basically creates a SQL query.
+        Cursor result = db.rawQuery("select * from "+ TABLE_NAME,null);
+        //we will now return the instance of this cursor, which is "result"
+        return result;
+
+    }
+    //creating a method that will update all data in our database, using 4 args all of which are
+//string, they are id, name, surname and marks
+    public boolean updateData(String email, String fname, String lname, String pass){
+        //creating an instance of the database class firstly to allow us to get all the data
+        SQLiteDatabase db = this.getWritableDatabase();
+        //now we need to create an instance of the class ContentValue
+        ContentValues contentValues = new ContentValues();
+        //we will now take this contentValues instance and insert it into the data columns
+        //the first arg is the column name itself, the second is the data itself.
+        contentValues.put(COL_2, email);
+        contentValues.put(COL_3, fname);
+        contentValues.put(COL_4, lname);
+        contentValues.put(COL_5, pass);
+
+        //the below update method will update any args you pass through here
+        //the first argument is the table name itself, the second is the contentValues, the third
+        //is the condition you want to impose, such as "ID = ?" where the ? is the ID provided. The
+        //fourth arg is the String[] array
+        db.update(TABLE_NAME, contentValues, "EMAIL = ?", new String[] {email});
+        //we will return true to see if the data is really updated or not
+        return true;
+    }
+
+    public Integer deleteData(String email){
+        //creating an instance of the database class firstly to allow us to get all the data
+        SQLiteDatabase db = this.getWritableDatabase();
+        //calling the delete function on our db instance. It takes 3 args, 1st is the name of the
+        //table, the second is the ID number represented by "ID = ?" and the third arg is the String
+        //Array[] of the argument type id. The return below returns the integer of the ID
+        return db.delete(TABLE_NAME, "EMAIL = ?", new String[] {email});
+
+    }
+
+    public boolean hasObject(String email){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String selectString = "SELECT * FROM " + TABLE_NAME + " WHERE " + COL_2 + "= ?";
+
+        Cursor cursor = db.rawQuery(selectString,new String[]{email});
+        boolean exist;
+        if(cursor.getCount()>0){
+            exist=true;
+        } else {
+            exist=false;
+        }
+        db.close();
+        cursor.close();
+
+        return exist;
+    }
+
+    public boolean hasObject2(String pass){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String selectString = "SELECT * FROM " + TABLE_NAME + " WHERE " + COL_5 + "= ?";
+        Cursor cursor = db.rawQuery(selectString,new String[]{pass});
+        boolean exist;
+        if(cursor.getCount()>0){
+            exist=true;
+        } else {
+            exist=false;
+        }
+        db.close();
+        cursor.close();
+
+        return exist;
+    }
+
+//    public String getObjectKey(String email){
+//        SQLiteDatabase db = this.getWritableDatabase();
+//        String selectString = "SELECT ID FROM " + TABLE_NAME + "WHERE " + COL_2 + "= ?";
+//
+//
+//    }
+
 }
--- FitAid App/app/src/main/java/com/example/brad/fitaid/LoginActivity.java
+++ FitAid App/app/src/main/java/com/example/brad/fitaid/LoginActivity.java
@@ -7,15 +7,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
 
-    private EditText name;
+    DatabaseHelper myDb;
+    private EditText email;
     private EditText password;
     private Button enter;
     private Button login;
-    int counter = 5;
+  //  int counter = 5;
     private TextView info;
 
 
@@ -37,17 +39,17 @@ public class LoginActivity extends AppCompatActivity {
       } );
   }
 
-  public void init1(){
-      login = findViewById( R.id.login );
-      login.setOnClickListener( new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-//              validate( name.getText().toString(), password.getText().toString() );
-              Intent go2 = new Intent (LoginActivity.this, MainActivity.class);
-              startActivity(go2);
-          }
-      } );
-  }
+//  public void init1(){
+//      login = findViewById( R.id.login );
+//      login.setOnClickListener( new View.OnClickListener() {
+//          @Override
+//          public void onClick(View view) {
+////              validate( name.getText().toString(), password.getText().toString() );
+//              Intent go2 = new Intent (LoginActivity.this, MainActivity.class);
+//              startActivity(go2);
+//          }
+//      } );
+//  }
 
 //    public void validate(String userName, String password){
 //        info = findViewById(R.id.info);
@@ -73,15 +75,41 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
 
-        name = findViewById( R.id.name );
+        myDb = new DatabaseHelper( this );
+
+        email = findViewById( R.id.email_login );
         password = findViewById( R.id.password );
         enter = findViewById( R.id.enter );
         login = findViewById( R.id.login );
         info = findViewById(R.id.info);
 
+
+       signIn();
+
+
       //  info.setText( "No of attempts remaining: 5" );
     init();
-    init1();
+    //init1();
+    }
+
+
+
+    public void signIn(){
+      login.setOnClickListener( new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              boolean recordExists = myDb.hasObject( email.getText().toString() );
+              boolean passExists = myDb.hasObject2( password.getText().toString() );
+              if ((recordExists==true) && (passExists==true)){
+
+                  Intent intentsignin = new Intent(getApplicationContext(), MainActivity.class);
+                  Toast.makeText( getApplicationContext(), "Login successful, redirection to main page", Toast.LENGTH_LONG).show();
+                  startActivity( intentsignin );
+              } else{
+                  Toast.makeText(getApplicationContext(), "Invalid email or password, please try again", Toast.LENGTH_LONG).show();
+              }
+          }
+      } );
     }
 
 }
