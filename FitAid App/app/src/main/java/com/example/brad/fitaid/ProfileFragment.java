24bf09942af4d514d5e69ea5d182612405dd72ba
Bradford Mashlum <bmashlum@gmail.com>
2018-10-28 00:51:17 -0400

    Profile page now auto-loads current users most recent stats

--- FitAid App/app/src/main/java/com/example/brad/fitaid/ProfileFragment.java
+++ FitAid App/app/src/main/java/com/example/brad/fitaid/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.brad.fitaid;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
@@ -12,8 +13,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -32,13 +38,15 @@ import java.util.Set;
 public class ProfileFragment extends Fragment {
     //private String currentUser = SigninActivity2.userId;
 
-    final String email = SigninActivity2.userId.replaceAll("\\.","");
+    HashMap<String,Double> statMap;
+    final String email = SigninActivity2.userId.replaceAll("\\.", "");
     final String date_n = new SimpleDateFormat("M,dd,yyyy", Locale.getDefault()).format(new Date());
     private ArrayList<Double> inputText = new ArrayList<>();
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference ref = database.getReference("/" + email + "/" + date_n);
-    TextInputEditText etWeight,etBodyFat,etAge,etChest,etLeftArm,etRightArm,etWaist,etLeftLeg,etRightLeg,etLeftCalf,etRightCalf;
-    double weight,bodyFat,age,chest,leftArm,rightArm,waist,leftLeg,rightLeg,leftCalf,rightCalf;
+    DatabaseReference ref2 = database.getReference("/" + email);
+    TextInputEditText etWeight, etBodyFat, etAge, etChest, etLeftArm, etRightArm, etWaist, etLeftLeg, etRightLeg, etLeftCalf, etRightCalf;
+    double weight, bodyFat, age, chest, leftArm, rightArm, waist, leftLeg, rightLeg, leftCalf, rightCalf;
     Button btnSave;
 
     public ProfileFragment() {
@@ -54,6 +62,7 @@ public class ProfileFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
         btnSave = v.findViewById(R.id.btn_Save);
+
         etWeight = v.findViewById(R.id.inputWeight);
         etBodyFat = v.findViewById(R.id.inputFat);
         etAge = v.findViewById(R.id.inputAge);
@@ -66,6 +75,36 @@ public class ProfileFragment extends Fragment {
         etLeftCalf = v.findViewById(R.id.inputCalfL);
         etRightCalf = v.findViewById(R.id.inputCalfR);
 
+        Query lastQuery = ref2.orderByKey().limitToLast(1);
+        lastQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot data : dataSnapshot.getChildren())
+                {
+                     statMap = (HashMap) data.getValue();
+                     etWeight.setText(String.valueOf(statMap.get("Weight")));
+                     etBodyFat.setText(String.valueOf(statMap.get("Body Fat")));
+                     etAge.setText(String.valueOf(statMap.get("Age")));
+                     etChest.setText(String.valueOf(statMap.get("Chest")));
+                     etLeftArm.setText(String.valueOf(statMap.get("Left Arm")));
+                     etRightArm.setText(String.valueOf(statMap.get("Right Arm")));
+                     etWaist.setText(String.valueOf(statMap.get("Waist")));
+                     etLeftCalf.setText(String.valueOf(statMap.get("Left Calf")));
+                     etRightCalf.setText(String.valueOf(statMap.get("Right Calf")));
+                     etLeftLeg.setText(String.valueOf(statMap.get("Left Leg")));
+                     etRightLeg.setText(String.valueOf(statMap.get("Right Leg")));
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //
 
 
         btnSave.setOnClickListener(new View.OnClickListener() {
@@ -84,8 +123,7 @@ public class ProfileFragment extends Fragment {
                     rightLeg = Double.parseDouble(etRightLeg.getText().toString());
                     leftCalf = Double.parseDouble(etLeftCalf.getText().toString());
                     rightCalf = Double.parseDouble(etRightCalf.getText().toString());
-                }
-                catch(NumberFormatException e) {
+                } catch (NumberFormatException e) {
                     System.out.println("EMPTY STRING");
                 }
 
@@ -105,17 +143,17 @@ public class ProfileFragment extends Fragment {
 
                 LinkedHashMap<String, Double> exerciseMap = new LinkedHashMap<String, Double>();
 
-                exerciseMap.put("Weight",inputText.get(0));
-                exerciseMap.put("Body Fat",inputText.get(1));
-                exerciseMap.put("Age",inputText.get(2));
-                exerciseMap.put("Chest",inputText.get(3));
-                exerciseMap.put("Left Arm",inputText.get(4));
-                exerciseMap.put("Right Arm",inputText.get(5));
-                exerciseMap.put("Waist",inputText.get(6));
-                exerciseMap.put("Left Leg",inputText.get(7));
-                exerciseMap.put("Right Leg",inputText.get(8));
-                exerciseMap.put("Left Calf",inputText.get(9));
-                exerciseMap.put("Right Calf",inputText.get(10));
+                exerciseMap.put("Weight", inputText.get(0));
+                exerciseMap.put("Body Fat", inputText.get(1));
+                exerciseMap.put("Age", inputText.get(2));
+                exerciseMap.put("Chest", inputText.get(3));
+                exerciseMap.put("Left Arm", inputText.get(4));
+                exerciseMap.put("Right Arm", inputText.get(5));
+                exerciseMap.put("Waist", inputText.get(6));
+                exerciseMap.put("Left Leg", inputText.get(7));
+                exerciseMap.put("Right Leg", inputText.get(8));
+                exerciseMap.put("Left Calf", inputText.get(9));
+                exerciseMap.put("Right Calf", inputText.get(10));
 
                 ref.setValue(exerciseMap);
 
@@ -125,11 +163,8 @@ public class ProfileFragment extends Fragment {
         });
 
 
-
         return v;
     }
-
-
 
 
 }
